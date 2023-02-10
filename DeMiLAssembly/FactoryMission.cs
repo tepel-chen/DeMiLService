e9b977395162802301f867a5918d4d390f4fa285
tepel-chen <tepelchen501@gmail.com>
2023-02-10 12:21:16 +0900

    Add component pools info in missions data

--- DeMiLAssembly/FactoryMission.cs
+++ DeMiLAssembly/FactoryMission.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assets.Scripts.Missions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -8,9 +9,10 @@ namespace DeMiLService
 {
     class FactoryMission
     {
-        private const string MULTIPLE_BOMBS_TYPE_NAME = "FactoryAssembly.MultipleBombsInterface";
         private static Type _FactoryGameModePickerType;
         private static MethodInfo _UpdateCompatibleMissionsMethod;
+        private static MethodInfo _GetGameModeForMissionMethod;
+        private static MethodInfo _GetFriendlyNameMethod;
 
         public static bool IsInstalled => _FactoryGameModePickerType != null;
 
@@ -22,12 +24,34 @@ namespace DeMiLService
                     "UpdateCompatibleMissions", 
                     BindingFlags.Static | BindingFlags.NonPublic
                 );
+            _GetGameModeForMissionMethod =
+                _FactoryGameModePickerType.GetMethod(
+                    "GetGameModeForMission",
+                    BindingFlags.Static | BindingFlags.NonPublic,
+                    null,
+                    new Type[] { typeof(Mission) },
+                    null
+                );
+            _GetFriendlyNameMethod =
+                _FactoryGameModePickerType.GetMethod(
+                    "GetFriendlyName",
+                    BindingFlags.Static | BindingFlags.NonPublic
+                );
         }
 
         public static void UpdateCompatibleMissions()
         {
             if (!IsInstalled) return ;
             _UpdateCompatibleMissionsMethod.Invoke(null, null);
+        }
+
+        public static string GetGameMode(Mission mission)
+        {
+            if (!IsInstalled) return null;
+            object gamemode = _GetGameModeForMissionMethod.Invoke(null, new object[] { mission });
+            object result = _GetFriendlyNameMethod.Invoke(null, new object[] { gamemode });
+            if (result == null) return null;
+            return (string)result;
         }
     }
 }
--- DeMiLAssembly/MissionData.cs
+++ DeMiLAssembly/MissionData.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Assets.Scripts.Missions;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,12 +29,22 @@ namespace DeMiLService
                 Missions = mod.GetValue<List<ModMission>>("missions")
                 .Select(mission =>
                 {
+                    var multipleMission = MultipleBombsMissionDetails.ReadMission(mission);
                     var m = new MissionData
                     {
                         SteamID = steamID,
                         MissionID = mission.ID,
                         Title = Localization.GetLocalizedString(mission.DisplayNameTerm),
                         Description = Localization.GetLocalizedString(mission.DescriptionTerm),
+                        BombCount = multipleMission.BombCount,
+                        BombData = Enumerable.Range(0, multipleMission.BombCount).Select(i => {
+                            if(multipleMission.GeneratorSettings.TryGetValue(i, out GeneratorSetting setting))
+                            {
+                                return BombData.GetBombData(setting);
+                            }
+                            return null;
+                        }).ToArray(),
+                        FactoryMode = FactoryMission.GetGameMode(mission),
                         Port = port
                     };
                     return m;
@@ -53,7 +64,73 @@ namespace DeMiLService
         public string Title;
         public string MissionID;
         public string Description;
+        public string FactoryMode;
+        public BombData[] BombData;
+        public int BombCount;
         public string StartURL => $"http://localhost:{Port}/startMission?steamID={SteamID}&missionID={MissionID}";
+    }
+
+    [JsonObject]
+    class BombData
+    {
+        public float TimeLimit;
+        public int NumStrikes;
+        public int TimeBeforeNeedyActivation;
+        public bool FrontFaceOnly;
+        public int OptionalWidgetCount;
+        public string[][] ComponentPools;
+        public static BombData GetBombData(GeneratorSetting generator)
+        {
+            return new BombData
+            {
+                TimeLimit = generator.TimeLimit,
+                NumStrikes = generator.NumStrikes,
+                TimeBeforeNeedyActivation = generator.TimeBeforeNeedyActivation,
+                FrontFaceOnly = generator.FrontFaceOnly,
+                OptionalWidgetCount = generator.OptionalWidgetCount,
+                ComponentPools = generator.ComponentPools.SelectMany(pool =>
+                {
+                    string specialComponent = GetSpecialComponent(pool.SpecialComponentType, pool.AllowedSources);
+                    if (specialComponent != null)
+                    {
+                        return Enumerable.Repeat(new string[] { specialComponent }, pool.Count);
+                    }
+                    List<string> component = pool.ComponentTypes.Select(c => c.ToString()).ToList();
+                    component.AddRange(pool.ModTypes);
+                    return Enumerable.Repeat(component.ToArray(), pool.Count);
+
+                }).ToArray()
+            };
+        }
+
+        static string GetSpecialComponent(SpecialComponentTypeEnum type, ComponentPool.ComponentSource source)
+        {
+            if (type == SpecialComponentTypeEnum.ALL_NEEDY && source == (ComponentPool.ComponentSource.Base | ComponentPool.ComponentSource.Mods))
+            {
+                return "ALL_NEEDY";
+            }
+            if (type == SpecialComponentTypeEnum.ALL_SOLVABLE && source == (ComponentPool.ComponentSource.Base | ComponentPool.ComponentSource.Mods))
+            {
+                return "ALL_SOLVABLE";
+            }
+            if (type == SpecialComponentTypeEnum.ALL_NEEDY && source == ComponentPool.ComponentSource.Base)
+            {
+                return "ALL_VANILLA_NEEDY";
+            }
+            if (type == SpecialComponentTypeEnum.ALL_SOLVABLE && source == ComponentPool.ComponentSource.Base)
+            {
+                return "ALL_VANILLA";
+            }
+            if (type == SpecialComponentTypeEnum.ALL_NEEDY && source == ComponentPool.ComponentSource.Mods)
+            {
+                return "ALL_MODS_NEEDY";
+            }
+            if (type == SpecialComponentTypeEnum.ALL_SOLVABLE && source == ComponentPool.ComponentSource.Mods)
+            {
+                return "ALL_MODS";
+            }
+            return null;
+        }
     }
 
 
